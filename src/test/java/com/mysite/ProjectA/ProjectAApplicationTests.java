a311d09507dd43753fa84d5c28ffbc1e193ecf31
DESKTOP-DAIRV42\차지훈 <jihoon9998@naver.com>
2024-09-23 00:36:00 +0900

    first commit

---
+++ src/test/java/com/mysite/ProjectA/ProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.ProjectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
